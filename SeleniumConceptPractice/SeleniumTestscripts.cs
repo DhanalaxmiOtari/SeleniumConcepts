ce30aa240f07277734371931fa33058ffd3966dc
Dhanalaxmi Otari <dhanalaxmi.otari@instem.com>
2022-02-09 18:39:28 +0530

    dhanu

---
+++ SeleniumConceptPractice/SeleniumTestscripts.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+namespace SeleniumConceptPractice
+{
+
+    [TestClass]
+    public class SeleniumTestscipts
+    {
+        public static string title = "Testing Controls";
+        public static string URL = "http://www.uitestpractice.com/Students/Switchto";
+        // Selenium Concept - WindowsHandles
+        [TestMethod]
+        public void multipleBrowser()
+        {
+            WebHelp.launchBrowser();
+            WebHelp.openBrowser(URL,title);
+            Console.WriteLine("count = "+WebHelp.WebDriver.WindowHandles.Count);
+            foreach(var n in WebHelp.WebDriver.WindowHandles)
+            {
+                Console.WriteLine("WebHelp.WebDriver.WindowHandles = "+WebHelp.WebDriver.CurrentWindowHandle);
+            }
+            WebHelp.WebDriver.FindElement(By.LinkText("Opens in a new window")).Click();
+            Thread.Sleep(5000);
+            Console.WriteLine("count = " + WebHelp.WebDriver.WindowHandles.Count);
+            foreach (var n in WebHelp.WebDriver.WindowHandles)
+            {
+                Console.WriteLine("WebHelp.WebDriver.WindowHandles = " + WebHelp.WebDriver.CurrentWindowHandle);
+            }
+            //WebHelp.WebDriver.SwitchTo().Window(title);
+            WebHelp.WebDriver.SwitchTo().Window(WebHelp.WebDriver.WindowHandles[1]);
+            Console.WriteLine(WebHelp.WebDriver.FindElement(By.Id("draggable")).Text);
+            Thread.Sleep(5000);
+            WebHelp.WebDriver.SwitchTo().Window(WebHelp.WebDriver.WindowHandles[0]);
+            Console.WriteLine(WebHelp.WebDriver.FindElement(By.LinkText("Opens in a new window")).Text);
+
+            WebHelp.TearDown();
+        }
+        
+        [TestMethod]
+        public static void iJavaScrinptMethodsTests()
+        {
+
+            WebHelp.launchBrowser();
+            WebHelp.WebDriver.Navigate().GoToUrl("https://designsystem.digital.gov/components/radio-buttons/");
+
+
+        }
+        
+
+    }
+}
---
+++ SeleniumConceptPractice/WebHelp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumConceptPractice
+{
+    public static class WebHelp
+    {
+        public static IWebDriver WebDriver;
+
+        //Selenium Concept => WebDriverManager
+        public static void launchBrowser()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            WebDriver = new ChromeDriver();
+        }
+
+        //[TearDown]
+        public static void TearDown()
+        {
+            WebDriver.Quit();
+        }
+
+        //[Test]
+        public  static void openBrowser(string URL, string title)
+        {
+            WebDriver.Navigate().GoToUrl(URL);
+            Assert.IsTrue(WebDriver.Title.Contains(title));
+        }
+    }
+}
